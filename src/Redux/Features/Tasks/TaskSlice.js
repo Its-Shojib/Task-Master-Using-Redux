4d0f2ec30e5530f7faa10e23024335d8a8544aec
Md Shojib Hossain <mdshojib922@gmail.com>
2024-02-22 11:16:16 +0600

    Task Slice added

--- src/Redux/Features/Tasks/TaskSlice.js
+++ src/Redux/Features/Tasks/TaskSlice.js
@@ -1,21 +1,29 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+const initialState = {
+    tasks: [],
+}
+
 export const tasksSlice = createSlice({
-    name: 'tasks',
-    initialState: [],
+    name: 'tasksSlice',
+    initialState,
     reducers: {
-        addTask: (state, action) => {
-            state.push(action.payload);
-        },
-        deleteTask: (state, action) => {
-            state = state.filter(task => task.id!== action.payload);
-        },
-        updateStatus: (state, action) => {
-            state.forEach(task => {
-                if (task.id === action.payload.id) {
-                    task.status = action.payload.status;
-                }
-            });
-        }
+        // addTask: (state, action) => {
+        //     state.push(action.payload);
+        // },
+        // deleteTask: (state, action) => {
+        //     state = state.filter(task => task.id!== action.payload);
+        // },
+        // updateStatus: (state, action) => {
+        //     state.forEach(task => {
+        //         if (task.id === action.payload.id) {
+        //             task.status = action.payload.status;
+        //         }
+        //     });
+        // }
     }
-})
+});
+
+// export const {addTask, deleteTask, updateStatus} = tasksSlice.actions;
+
+export default tasksSlice.reducer;
