092ed06266ca9b13857aa5df3789cc9e2cc46627
Md Shojib Hossain <mdshojib922@gmail.com>
2024-02-25 12:52:14 +0600

    User Updated

--- src/Redux/Features/Users/UserSlice.js
+++ src/Redux/Features/Users/UserSlice.js
@@ -9,25 +9,25 @@ export const userSlice = createSlice({
     name: 'userSlice',
     initialState,
     reducers: {
-        addUser: (state, { payload }) => {
-            if (state.users.length === 0) {
-                state.users.push({ id: 1, ...payload });
-            } else {
-                // let last = state.users.at(-1); //find the last user in the list at(-1)
-                // state.users.push({ id: last.id + 1,...payload });
-                state.users.push({ id: state.users.length + 1, ...payload });
-            }
-        },
-        deleteUser: (state, { payload }) => {
-            state.users = state.users.filter((item) => item.id !== payload);
-        },
-        updateUser: (state, { payload }) => {
-            state?.users?.forEach(user => {
-                if (user.id === payload.id) {
-                    user.name = payload.name;
-                }
-            });
-        },
+        // addUser: (state, { payload }) => {
+        //     if (state.users.length === 0) {
+        //         state.users.push({ id: 1, ...payload });
+        //     } else {
+        //         // let last = state.users.at(-1); //find the last user in the list at(-1)
+        //         // state.users.push({ id: last.id + 1,...payload });
+        //         state.users.push({ id: state.users.length + 1, ...payload });
+        //     }
+        // },
+        // deleteUser: (state, { payload }) => {
+        //     state.users = state.users.filter((item) => item.id !== payload);
+        // },
+        // updateUser: (state, { payload }) => {
+        //     state?.users?.forEach(user => {
+        //         if (user.id === payload.id) {
+        //             user.name = payload.name;
+        //         }
+        //     });
+        // },
     }
 });
 
