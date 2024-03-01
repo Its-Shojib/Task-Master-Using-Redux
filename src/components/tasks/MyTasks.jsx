333c9d3c357e1ec787e210c8c63c74ebaa04b05b
Md Shojib Hossain <mdshojib922@gmail.com>
2024-03-01 19:10:55 +0600

    Item Added

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -19,16 +19,16 @@ const MyTasks = () => {
 
   }, [dispatch, userName, tasks]);
 
-  // const item = {
-  //   id: 1,
-  //   status: 'pending',
-  //   title: 'Remove Button',
-  //   description:
-  //     'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
-  //   date: '2023-08-28',
-  //   assignedTo: 'Mir Hussain',
-  //   priority: 'high',
-  // };
+  const item = {
+    id: 1,
+    status: 'pending',
+    title: 'Remove Button',
+    description:
+      'We need a remove button in our task card. Meke the button red and use Heroicon for tashbin icon.',
+    date: '2023-08-28',
+    assignedTo: 'Mir Hussain',
+    priority: 'high',
+  };
 
   return (
     <div>
